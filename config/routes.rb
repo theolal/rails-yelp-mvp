b9a48e909019f8374a2ffe8154bb62a1f03b3952
Theo Nazon <theonazon@gmail.com>
2018-02-14 15:50:57 +0100

    Ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get   'restaurants', to: 'restaurants#index', as: :restaurants
+  post  'restaurants', to: 'restaurants#create'
+  get   'restaurants/new', to: 'restaurants#new', as: :new_restaurant
+  get   'restaurants/:id', to: 'restaurants#show', as: :restaurant
+  get   'restaurants/:restaurant_id/reviews/new', to: 'reviews#new', as: :new_restaurant_review
+  get   'restaurants/:restaurant_id/reviews', to: 'reviews#index', as: :reviews
+  post  'restaurants/:restaurant_id/reviews', to: 'reviews#create'
+
 end
